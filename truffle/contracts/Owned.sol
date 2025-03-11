d63fd599348b6f96b6bc48a861b67de707496939
NERD777 <137885674+nerdnomerch@users.noreply.github.com>
2025-03-11 14:00:18 +0700

    node: inital contract call.

---
+++ truffle/contracts/Owned.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.0;
+
+interface OwnedInterface {
+    function getOwner() external view returns(address owner);
+    function changeOwner(address newOwner) external returns (bool success);
+}
+
+contract Owned is OwnedInterface {
+    
+    address private contractOwner;
+  
+    event LogOwnerChanged(
+        address newOwner);
+
+    modifier onlyOwner {
+        require(msg.sender == contractOwner);
+        _;
+    } 
+  
+    constructor() {
+        contractOwner = msg.sender;
+    }
+
+    function getOwner() public override view returns(address owner) {
+        return contractOwner;
+    }
+  
+    function changeOwner(address newOwner) 
+        public
+        override
+        onlyOwner 
+        returns(bool success) 
+    {
+        require(newOwner != address(0));
+        emit LogOwnerChanged(newOwner);
+        contractOwner = newOwner;
+        return true;
+    }
+}
