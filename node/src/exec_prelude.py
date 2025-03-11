90a820bb7de805299f950358905fdcb38561a481
NERD777 <137885674+nerdnomerch@users.noreply.github.com>
2025-03-11 14:18:21 +0700

    Add example, fix encoding problem.

---
+++ node/src/exec_prelude.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# __elcaro__imports__ = __builtins__.__dict__['__import__']
+# __elcaro__builtins__open = __builtins__.open
+# __elcaro__builtins__getattr = __builtins__.getattr
+# __elcaro__builtins__globals = __builtins__.globals
+# __elcaro__builtins__exec = __builtins__.exec
+# __elcaro__builtins__eval = __builtins__.eval
+# __elcaro__builtins__exit = __builtins__.exit
+# __elcaro__builtins__compile = __builtins__.compile
+
+import json
+import requests
+import decimal
+
+
+# def prevent_imports(name, globals=None, locals=None, fromlist=(), level=0):
+#     raise ImportError("module '%s' is restricted." % name)
+
+
+# __builtins__.__dict__['__import__'] = prevent_imports
+# __builtins__.open = None
+# __builtins__.getattr = None
+# __builtins__.globals = None
+# __builtins__.exec = None
+# __builtins__.eval = None
+# __builtins__.exit = None
+# __builtins__.compile = None
